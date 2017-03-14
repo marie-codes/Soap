19825904ef79ef5e954755bcf1e049e2a03c118a
mariejinpark <mariejinpark@gmail.com>
2017-03-13 20:11:40 -0400

    Init

---
+++ Soap/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Soap
+//
+//  Created by Marie Park on 3/13/17.
+//  Copyright © 2017 Marie Park. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
